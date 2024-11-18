1dbe8477dbc37f2fdfd25234089efa38997f7a1f
Steven-Liao24 <liaoxion@myvuw.ac.nz>
2024-11-18 22:04:19 +0800

    first

---
+++ app/utils/api.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+
+const getChannel = (user) => (
+  axios.get(`https://twitch-proxy.freecodecamp.rocks/helix/${user}`)
+    .then((user) => user.data)
+);
+
+const getStream = (user) => (
+  axios.get(`https://twitch-proxy.freecodecamp.rocks/helix/${user}` )
+    .then((user) => user.data)
+);
+const getChannelsData = (ch) => {
+  return axios.all([
+    getChannel(ch),
+    getStream(ch)
+  ]).then(function (data) {
+    var channel = data[0];
+    var streams = data[1];
+
+    return {
+      channel: channel,
+      streams: streams
+    }
+  });
+}
+const handleError = (error) => {
+  console.warn(error);
+  return null;
+}
+
+module.exports = {
+  fetchChannels: function (ch) {
+    return axios.all(ch.map(getChannelsData))
+      .then(channelData => channelData)
+      .catch(handleError);
+  },
+}
